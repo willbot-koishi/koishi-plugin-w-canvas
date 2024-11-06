92dc151e27e7c1df27af3fa722515f330ea21107
ForkΨKILLET <fork_killet@outlook.com>
2024-11-06 19:35:43 +0800

    chore: partly implements `skia-canvas` service interface

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,35 @@ class CanvasService extends Service {
     static readonly inject = [ 'node' ]
 
     public skia: typeof skia
-
+    public Canvas: typeof skia.Canvas
+    public Path2D: typeof skia.Path2D
+    public ImageData: typeof skia.ImageData
+    public Image: typeof skia.Image
+    public DOMPoint: typeof skia.DOMPoint
+    public DOMMatrix: typeof skia.DOMMatrix
+    public DOMRect: typeof skia.DOMRect
+    
     private oldImage: typeof Image
 
     public async start() {
         const skia = await this.ctx.node.safeImport<typeof skia>('@willbot-koishi/skia-canvas')
         this.skia = skia
+        void ({
+            Canvas: this.Canvas,
+            Path2D: this.Path2D,
+            ImageData: this.ImageData,
+            Image: this.Image,
+            DOMPoint: this.DOMPoint,
+            DOMMatrix: this.DOMMatrix,
+            DOMRect: this.DOMRect,
+        } = skia)
+
         this.oldImage = global.Image
         global.Image = skia.Image
+    }
+
+    public createCanvas(width: number, height: number, _svgFlag: any) {
+        return new this.Canvas(width, height)
     }
 
     public constructor(ctx: Context, public config: CanvasService.Config) {
